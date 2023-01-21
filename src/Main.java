607a97224d1a38060de7ace0ebc09cfd2023a151
Timur <mirtimoha@gmail.com>
2023-01-21 08:37:07 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //first task
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //second task
+        for (int i = 10; i <= 1; i--) {
+            System.out.println(i);
+        }
+        //third task
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        //fourth task
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        //fifth task
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " year is leap");
+        }
+        //sixth task
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        //seventh task
+        for (int i = 1; i <=512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 }
